5354573851516eed72347b53a2c9d65c45cb1197
paoloLigsay <paolomartinligsay@gmail.com>
2024-02-28 16:48:06 +0800

    Feat: added a chatbot feature

---
+++ models/chat.ts
@@ -0,0 +1,8 @@
+type User = "user" | "assistant" | "system"
+
+export interface Message {
+  id: string;
+  created_at: string;
+  role: User;
+  content: string;
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ import AddTodoModal from '../components/AddTodoModal';
 import UpdateTodoModal from '@/components/UpdateTodoModal';
 import { Todo } from '@/models/todo';
 import ListItem from '@/components/ListItem';
+import Chatbot from '@/components/Chatbot';
 
 function index() {
   const [todos, setTodos] = useState<Todo[]>([]);
@@ -31,50 +32,6 @@ function index() {
     fetchData();
   }, [])
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch('http://localhost:8080/api/generate', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({
-            input: 'Say this is a test', // Your input data
-          }),
-        });
-  
-        if (response && response.body) {
-            const reader = response.body.getReader();
-            const decoder = new TextDecoder();
-            let completeResponse = "";
-    
-            while (true) {
-              const { done, value } = await reader.read();
-    
-              if (done) {
-                console.log("DONE!");
-                break;
-              }
-    
-              const chunkValue = decoder.decode(value);
-              completeResponse += chunkValue;
-    
-              // Update your state or perform any action with the chunk data
-              console.log("response chunk: ", chunkValue);
-            }
-    
-            // You can also use completeResponse after the stream is finished
-            console.log("complete response: ", completeResponse);
-        }
-      } catch (e) {
-        console.log(e);
-      }
-    };
-
-  fetchData();
-  }, []);
-
   return (
     <div className='mx-auto mt-[120px] w-[60vw]'>
       <AddTodoModal
@@ -100,7 +57,7 @@ function index() {
       <ul className='mt-[64px]'>
         {todos.map((todo: Todo) => <ListItem key={todo.id} todo={todo} setTodos={setTodos} setTodoToUpdate={setTodoToUpdate} setOpenUpdateTodoModal={setOpenUpdateTodoModal} />)}
       </ul>
-      {/* <Chatbot /> */}
+      <Chatbot />
     </div>
   )
 }
